5721a670aa7ea571162b289f6e2fee592f1aa46f
Soramichi Akiyama <akiyama@m.soramichi.jp>
2018-11-05 23:51:50 +0900

    add prefetcher

--- mmap_allocator.h
+++ mmap_allocator.h
@@ -13,4 +13,15 @@ void* mm_calloc_approximate(size_t nmemb, size_t size);
 void* mm_realloc_approximate(void* ptr, size_t size);
 void mm_free_approximate(void* ptr);
 
+// c_region and a_region must be on the cache!! (how can we guarantee this??)
+#define fetch(c_value, c_ptr, c_region, a_region) do {	\
+    int __index = ((c_ptr) - (c_region));		\
+    typeof(a_region) __target = (a_region) + __index;	\
+    __asm__ __volatile__("prefetcht0 (%0);"		\
+                         :		      		\
+                         :"r"(__target)			\
+                        );				\
+    c_value = *(c_ptr);					\
+  } while(0)
+
 #endif
